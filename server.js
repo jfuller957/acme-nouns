5c345a98dd7d59f5603b3184f81bfbe44549b838
James Fuller <jfuller957@gmail.com>
2019-09-16 20:22:07 -0700

    able to single out an id and get it to populate

--- server.js
+++ server.js
@@ -2,4 +2,35 @@ const express = require('express');
 const db = require('./db');
 const app = express();
 const { Person, Place, Thing } = db.models;
+const path = require('path');
 
+app.get('/api/persons', (req, res, next)=> {
+  Person.findAll()
+    .then( persons => res.send(persons))
+    .catch(next);
+});
+
+app.get('/api/persons/:id', (req, res, next)=> {
+  Person.findByPk(req.params.id)
+    .then( person => res.send(person))
+    .catch(next);
+});
+
+app.get('/api/places', (req, res, next)=> {
+  Place.findAll()
+    .then( places => res.send(places))
+    .catch(next);
+});
+
+app.get('/api/things', (req, res, next)=> {
+  Thing.findAll()
+    .then( things => res.send(things))
+    .catch(next);
+});
+
+const port = process.env.PORT || 3000;
+
+db.syncAndSeed()
+  .then(()=> {
+    app.listen(port, ()=> console.log(`listening on port ${port}`));
+  });
