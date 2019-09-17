af6b793431a72bd399c72107e14f7a1d5900fc21
James Fuller <jfuller957@gmail.com>
2019-09-16 19:58:11 -0700

    got names to show and tables to populate

--- db.js
+++ db.js
@@ -1,15 +1,61 @@
 const Sequelize = require('sequelize');
+const { STRING, UUID, UUIDV4 } = Sequelize;
 
-const conn = new Sequelize('postgres://localhost/acme-nouns');
 
-const Person = conn.define('person', {});
+const conn = new Sequelize('postgres://localhost/acme_nouns');
 
-const Place = conn.define('place', {});
+const Person = conn.define('person', {
+  name: {
+    type: STRING,
+    unique: true,
+    notNull: true,
+    notEmpty: true
+  }
+});
 
-const Thing = conn.define('thing', {});
+const Place = conn.define('place', {
+  name: {
+    type: STRING,
+    unique: true,
+    notNull: true,
+    notEmpty: true
+  }
+});
+
+const Thing = conn.define('thing', {
+  name: {
+    type: STRING,
+    unique: true,
+    notNull: true,
+    notEmpty: true
+  }
+});
 
 const syncAndSeed = async()=> {
   await conn.sync({ force: true });
+
+  const persons = [
+    { name: 'Larry' },
+    { name: 'Curly' },
+    { name: 'Moe' }
+  ];
+
+  const places = [
+    { name: 'Los Angeles' },
+    { name: 'Oceanside' },
+    { name: 'DEATH VALLEY' }
+  ];
+
+  const things = [
+    { name: 'patience' },
+    { name: 'time' },
+    { name: 'headshot' }
+  ];
+
+  await Promise.all(persons.map( person => Person.create(person)));
+  await Promise.all(places.map( place => Place.create(place)));
+  await Promise.all(things.map( thing => Thing.create(thing)));
+
 };
 
 syncAndSeed();
